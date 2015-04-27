f09726ae386202d4d72b58ba6e4185d25460ada9
Mani Dhayal <mgage.d@gmail.com>
2015-04-27 22:00:36 +0530

    Added breath first search algorithm along with graph and node class

---
+++ breath_first_search.rb
@@ -0,0 +1,43 @@
+class BreathFirstSearch
+  def initialize(graph, source_node)
+    @graph = graph
+    @node = source_node
+    @visited = []
+    @edge_to = {}
+
+    bfs(source_node)
+  end
+
+  def shortest_path_to(node)
+    return unless has_path_to?(node)
+    path = []
+
+    while(node != @node) do
+      path.unshift(node)
+      node = @edge_to[node]
+    end
+
+    path.unshift(@node)
+  end
+
+  private
+  def bfs(node)
+    queue = []
+    queue << node
+    @visited << node
+
+    while queue.any?
+      current_node = queue.shift
+      current_node.adjacents.each do |adjacent_node|
+        next if @visited.include?(adjacent_node)
+        queue << adjacent_node
+        @visited << adjacent_node
+        @edge_to[adjacent_node] = current_node
+      end
+    end
+  end
+
+  def has_path_to?(node)
+    @visited.include?(node)
+  end
+end
